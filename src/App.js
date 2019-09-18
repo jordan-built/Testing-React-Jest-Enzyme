98cf139ad83f97e6b4ad0d13d28eeae32af81d7b
Jordan Marshall <jordan.marshall@getbuilt.com>
2019-09-18 09:23:17 -0500

    header and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import Header from './components/header';
+import './app.scss';
 
 function App() {
   return (
     <div className="App">
-      Test
+      <Header/>
     </div>
   );
 }
